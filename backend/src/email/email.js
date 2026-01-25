10e02b1a39aeacd13bcb36ef49311e03aa6b03ae
offshivam87 <codewithtrapnog@gmail.com>
2026-01-25 11:35:03 +0530

    nodemailer fix

--- backend/src/email/email.js
+++ backend/src/email/email.js
@@ -2,13 +2,10 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    type: 'OAuth2',
     user: process.env.EMAIL_USER,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
